d14459d98f1aef43e5cae413c102a4c75cebbcb2
zippy-waluse <zippywaluse@gmail.com>
2024-03-21 21:59:41 +0300

    added js menu

---
+++ menu.js
@@ -0,0 +1,46 @@
+
+
+var drink = prompt("What is your choice of drink")
+
+const menu = {
+    juice: [
+    { name: "Mango", },
+    { name: "Pineapple",  },
+    { name: "Orange juice",},
+    { name: "Guava juice",},
+    {name: "Passion fruit juice",},
+    { name: "Grape juice",},
+    {name: "Avocado juice",},
+ ],
+ vegetableJuice: [
+    { name: "kale juice",  },
+    { name: "Celary juice",  },
+    { name: "Carrot juice",},
+    { name: "Beatroote",},
+    {name: "Mint juice",},
+    { name:"Cucumber juice",},
+ ],
+ cocktail: [
+    { name: "Apple juice cocktail",  },
+    { name: "Indian mixed cocktail", },
+    { name: "Paloma cocktail", },
+    {name: "Ginger carrot cocktail",},
+    { name: "Salty dog",},
+    {name: "Strawberry orange cocktail",},
+   ( { name: "Mocktail"})
+ ],
+};
+
+
+
+
+
+// function myFunction(){
+//     let customer = prompt("Please enter drink of choice", "person")
+//     if (person !=null){
+       
+//     }
+//  }
+
+
+// console.log(menu)//
