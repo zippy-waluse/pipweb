2ad0c14f244a828941fa792245dfce2a546e7c38
zippy-waluse <zippywaluse@gmail.com>
2024-03-26 22:05:44 +0300

    added file

---
+++ searchmenu.py
@@ -0,0 +1,10 @@
+# juice = input("Enter you drink of choice: ")
+# print("We have  " + juice )
+
+
+<section >
+    <form action="" method="">          
+        <input type="search" placeholder="What are you looking for?">               
+        <button>Search</button>
+    </form>
+</section>
