215b1307d8dab29e23f364592f2520a9cbf8ae20
zippy-waluse <zippywaluse@gmail.com>
2024-03-27 22:36:57 +0300

    made changes

--- searchmenu.py
+++ searchmenu.py
@@ -1,10 +1,2 @@
-# juice = input("Enter you drink of choice: ")
-# print("We have  " + juice )
-
-
-<section >
-    <form action="" method="">          
-        <input type="search" placeholder="What are you looking for?">               
-        <button>Search</button>
-    </form>
-</section>
+username = input("Enter drink of choice:")
+print("Your drink is available!!" + username)
