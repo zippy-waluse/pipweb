340f11a4a11328eec12c1e90e1011790cfdb0033
zippy-waluse <zippywaluse@gmail.com>
2024-03-31 22:52:32 +0300

    made changes

--- searchmenu.py
+++ searchmenu.py
@@ -1,2 +1,55 @@
-# username = input("Enter drink of choice:")
-# print("Your drink is available!!" + username)
+# username = input("Enter drink of choice:")//
+# print("Your drink is available!!" + username)//
+
+# def juices():
+#     print("Select an option")
+#     print("1. Entry 1")
+#     print("2. Entry 2")
+#     print("3. Entry 3")
+
+# user_input = int(input("Enter ur num: "))
+# if user_input == 1:
+#     print("you have selected 1")
+# elif user_input == 2:
+#     print("You have selected 2")
+
+#     if __name__ =="_juices_":
+#         juices()
+
+# from requests import options
+
+
+def print_menu():
+    print('Welcome to my menu')
+    print("1. Option 1")
+    print("2. Option 2")
+    print("3. Option 3")
+    print("4. Done")
+
+    def juice():
+        
+            show_menu()
+            choice = input("Enter choice")
+
+            if choice == '1':
+                option1()
+            elif choice == '2':
+                option2()
+             
+            elif choice == '3':
+                 option3()
+            elif choice == '3':
+                 option3()
+            elif choice == '4':
+                 print("Done...")
+            else:
+                 print("Invalid choice")        
+        
+            
+
+
+   
+               
+              
+                 
+              
